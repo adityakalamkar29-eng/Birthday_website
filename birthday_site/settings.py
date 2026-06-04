d3ee66ea5e7ed706acb365cf7624c0a98ab912bb
Aditya kalamkar <adityakalamkar29@gmail.com>
2026-06-04 16:41:12 +0530

    fix2

--- birthday_site/settings.py
+++ birthday_site/settings.py
@@ -53,7 +53,7 @@ WSGI_APPLICATION = 'birthday_site.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
+        os.environ.get("DATABASE_URL")
     )
 }
 
